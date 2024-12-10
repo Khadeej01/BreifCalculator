cac740c9fcfe81277d28e7deee8c2c625c1665c7
khadijaDev <khadijaoukhlift@gmail.com>
2024-12-10 17:16:29 +0100

    I declared the variables

---
+++ breifcalculator.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("____the menu of our calculator____");
+        int operator, n1, n2;
+        double result;
+
+        
+    }
+
+    
+}
