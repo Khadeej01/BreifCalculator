5304cc171a0c804a0b74a7228f33197341836998
khadijaDev <khadijaoukhlift@gmail.com>
2024-12-11 11:12:23 +0100

    add function is finished

--- breifcalculator.java
+++ breifcalculator.java
@@ -6,7 +6,8 @@ public class Main {
         System.out.println("____the menu of our calculator____");
         int operator, n1, n2;
         double result;
-	 System.out.println("1: Add");
+           do{
+         System.out.println("1: Add");
         System.out.println("2: Subtract");
         System.out.println("3: Multiply");
         System.out.println("4: Divide");
@@ -15,8 +16,33 @@ public class Main {
         System.out.println("7: Factorial");
         System.out.println("8: Quit");
         System.out.print("Choose an operator: ");
+        Scanner sc = new Scanner(System.in);
+        operator = sc.nextInt();
 
-        
+            
+
+
+
+        switch (operator) {
+            case 1:
+               
+                System.out.print("Enter the first number :");
+                n1 = sc.nextInt();
+                System.out.print("Enter the second number :");
+                n2 = sc.nextInt();
+         
+                result = add(n1, n2);
+                System.out.println("The sum is: " + result);
+                break;
+                default:
+                System.out.println("Entered number is not valid");
+
     }
-
+            }while (operator !=1 );
+    }
+// Add two numbers
+    static int add(int n1, int n2) {
+        return n1 + n2;
+    }
 }
+
