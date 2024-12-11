f2b97192792cd50e92b699548bbf592b20e0811f
khadijaDev <khadijaoukhlift@gmail.com>
2024-12-11 10:28:45 +0100

    I've added the menu

--- breifcalculator.java
+++ breifcalculator.java
@@ -6,9 +6,17 @@ public class Main {
         System.out.println("____the menu of our calculator____");
         int operator, n1, n2;
         double result;
+	 System.out.println("1: Add");
+        System.out.println("2: Subtract");
+        System.out.println("3: Multiply");
+        System.out.println("4: Divide");
+        System.out.println("5: Power");
+        System.out.println("6: Sqrt");
+        System.out.println("7: Factorial");
+        System.out.println("8: Quit");
+        System.out.print("Choose an operator: ");
 
         
     }
 
-    
 }
